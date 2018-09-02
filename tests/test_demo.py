bd9f5c378323b0b8692cba59e04e27a03c24d270
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-09-02 14:54:01 +0700

    test case

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,3 +1,12 @@
 from odoo.tests import common
+import logging
+logger = logging.getLogger('product')
 
-# test gone here.
+_logger = logging.getLogger(__name__)
+
+class TestDemo(common.TransactionCase):
+  def test_import_single_product_cost(self):
+    test_import = self.env['magento.store'].import_single_product_cost('590010007130102')
+    _logger.debug('test start')
+    self.assertEqual(test_import, True) ## Fail / Success Depend on data working properly.
+    print('Your test was succesfull!')
